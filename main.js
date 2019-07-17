caedd722404644fe4e96e1c6e4f5ca7580692607
Nishant Singh <755083@student.tor.loc>
2019-07-17 16:11:03 -0400

    2 commit

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
+const { app, BrowserWindow } = electron;
+let mainWindow;
+// Listen for App to be Ready!
+app.on('ready', function() {
+    // create new Window
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({
+        pathname: path.join(_dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
 
-const { app, BrowerWindow } = electron;
+
+    }))
+});
